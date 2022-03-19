0bb02eb98ee2d897d9ca3d01d3a83d77510ad8f2
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-03-19 18:12:07 +0530

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=65;i<(n+65);i++)     {         for(j=0;j<n;j++)         {             printf("%c ",i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-int i,j,n;
-scanf("%d",&n);
-for(i=1;i<=n;i++)//1,2,3,4,5
-{
-for(j=1;j<=n-i+1;++j)
-//           5,4,3,2,1
-{
-printf("%d",j);
-}
-printf("
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=65;i<(n+65);i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%c ",i);
+        }
+        printf("
 ");
-}
+    }
 }
