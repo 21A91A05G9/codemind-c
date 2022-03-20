4fd63e7b3e7c46de3317e4dc601e247901458182
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-03-20 13:43:39 +0530

    #include<stdio.h> int main() { 	int n,i,a[100]; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	for(i=0;i<n/2;i++) 	{ 		printf("%d ",a[i]); 		printf("%d ",a[(n/2)+i]); 	} }

---
+++ _Shuffle_the_Array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a[100];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n/2;i++)
+	{
+		printf("%d ",a[i]);
+		printf("%d ",a[(n/2)+i]);
+	}
+}
