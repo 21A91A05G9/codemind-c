8c416a5f572e0f65e0e6eeaf57c89ae739c1e3cf
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-04-23 19:30:17 +0530

    #include<stdio.h> int main() {     long long int n,f=0,c=0;     scanf("%lld",&n);     while(n!=1)     {         f=0;         if(n%2==0)         {         f=1;         n=n/2;         }         else if(n%3==0)         {         f=1;         n=n/3;         }         else if(n%5==0)         {         f=1;         n=n/5;         }         if(f==0)         {         printf("Not Ugly Number");         c=1;         break;         }     }     if(c==0)     printf("Ugly Number"); }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,31 +1,33 @@
 #include<stdio.h>
 int main()
 {
-int n,x=0;
-  scanf("%d",&n);
-       while (n != 1) 
-       {  
-            if (n % 5 == 0) 
-            {  
-                n /= 5;  
-            } 
-            else if (n % 3 == 0) 
-            {  
-                n /= 3;  
-            } 
-            else if (n % 2 == 0) 
-            {  
-                n /= 2;  
-            } 
-            else 
-            {  
-                printf("Not Ugly Number"); 
-                x = 1;  
-                break;  
-            }  
-        } 
-		        if (x==0)
-		        { 
-                printf("Ugly Number");
-                }
+    long long int n,f=0,c=0;
+    scanf("%lld",&n);
+    while(n!=1)
+    {
+        f=0;
+        if(n%2==0)
+        {
+        f=1;
+        n=n/2;
+        }
+        else if(n%3==0)
+        {
+        f=1;
+        n=n/3;
+        }
+        else if(n%5==0)
+        {
+        f=1;
+        n=n/5;
+        }
+        if(f==0)
+        {
+        printf("Not Ugly Number");
+        c=1;
+        break;
+        }
+    }
+    if(c==0)
+    printf("Ugly Number");
 }
