9ba688b18de3453e4152f252e8382cbc9b26afb7
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-04-23 21:27:15 +0530

    #include<stdio.h> int fun(int n) {     int i;     if(n<=1) return 0;     for(i=2;i<n;i++)     {         if(n%i==0)         return 0;     }     return 1; } int main() {     int n,i,m;     scanf("%d%d",&n,&m);     for(i=n;i<=m;i++)     {         if(fun(i)){             printf("%d ",i);         }     } }

--- Generate_Prime_Numbers_In_A_Interval_.c
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
+int fun(int n)
+{
+    int i;
+    if(n<=1) return 0;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        return 0;
+    }
+    return 1;
+}
 int main()
 {
-	int n,m,i,c=0,j;
-	scanf("%d%d",&n,&m);
-	for(i=n;i<=m;i++)
-	{
-	    c=0;
-		for(j=2;j<i;j++)
-		{
-			if(i%j==0)
-			{
-				c++;
-			}
-		}
-		if(i!=1)
-		{
-		if(c==0)
-		{
-			printf("%d
+    int n,i,m;
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+        if(fun(i)){
+            printf("%d
 ",i);
-		}
-		}
-	}
+        }
+    }
 }
