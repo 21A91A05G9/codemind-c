7ecc31e6328be725e3a44781846730cbffe044ea
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-04-23 20:37:49 +0530

    #include<stdio.h> int div(int n) {     int i,s=1;     for(i=2;i<(n);i++)     {         if(n%i==0)         {            s+=i;         }     }     return s; } int main() {     int n,m,x,s;     scanf("%d",&n);     s=div(n);     if(s>n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int div(int n)
+{
+    int i,s=1;
+    for(i=2;i<(n);i++)
+    {
+        if(n%i==0)
+        {
+           s+=i; 
+        }
+    }
+    return s;
+}
+int main()
+{
+    int n,m,x,s;
+    scanf("%d",&n);
+    s=div(n);
+    if(s>n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
