7041d47f5e8c6921fb45a1440ea26e3f98f6a37a
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-04-28 14:43:09 +0530

    #include<stdio.h> #include<string.h> int main() {     char ch[100];     int i,s=0,x;     scanf("%s ",ch);     for(i=0;i<strlen(ch);i++)     {         x=ch[i]-48;         for(int j=0;j<=9;j++)         {             if(x==j)             {                 s+=x;                 break;             }         }     }     printf("%d",s); }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char ch[100];
+    int i,s=0,x;
+    scanf("%s ",ch);
+    for(i=0;i<strlen(ch);i++)
+    {
+        x=ch[i]-48;
+        for(int j=0;j<=9;j++)
+        {
+            if(x==j)
+            {
+                s+=x;
+                break;
+            }
+        }
+    }
+    printf("%d",s);
+}
